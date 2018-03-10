801d072f9bacf9cd2a80fe93748ba97a2d9df910
dinabogdan <bogdan.dina03@gmail.com>
2018-03-10 13:35:12 +0200

    second commit for AddNewRoute activity

--- app/src/main/java/com/freesoft/android_running_app/activities/AddNewRouteActivity.java
+++ app/src/main/java/com/freesoft/android_running_app/activities/AddNewRouteActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.freesoft.android_running_app.R;
 import com.freesoft.android_running_app.beans.Checkpoint;
@@ -34,14 +35,22 @@ public class AddNewRouteActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btn_start);
         btnStop = findViewById(R.id.btn_stop);
 
-        btnStart.setOnClickListener(new View.OnClickListener(){
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                final String REGEX = "^[a-zA-Z ]+$";
+                route = new Route();
+                if (tietRouteName.getText() == null || tietRouteName.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), R.string.route_name_not_complete_errorMsg, Toast.LENGTH_SHORT).show();
+                } else if (!tietRouteName.getText().toString().matches(REGEX)) {
+                    Toast.makeText(getApplicationContext(), R.string.route_name_invalid_errorMsg, Toast.LENGTH_SHORT).show();
+                } else {
+                    route.setRouteName(tietRouteName.getText().toString());
+                }
             }
         });
 
-        btnStop.setOnClickListener(new View.OnClickListener(){
+        btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
--- app/src/main/java/com/freesoft/android_running_app/beans/Route.java
+++ app/src/main/java/com/freesoft/android_running_app/beans/Route.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -15,6 +16,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 public class Route {
 
